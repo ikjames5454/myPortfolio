34af5fe245b0b6b7c4ef6caed1a8dfa2b41d5623
ikjames5454 <ikennajames03@gmail.com>
2023-11-17 13:35:02 +0100

    add

---
+++ portfolio/emailSender.js
@@ -0,0 +1,7 @@
+const fullName = document.getElementById("fullName");
+const title = document.getElementById("title");
+const sender = document.getElementById("senderEmail");
+const content = document.getElementById("content");
+function senderEmail(){
+    
+}
